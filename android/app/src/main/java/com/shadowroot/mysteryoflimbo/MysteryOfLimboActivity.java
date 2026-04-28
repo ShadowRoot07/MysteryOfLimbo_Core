833c0b2d0463904c1cbf1d8c5badfc8dd368b67e
ShadowRoot07 <angstart.1109@gmail.com>
2026-04-28 17:59:55 +0000

    refactor: Haciendo que la compilacion del binario de android funcione, try 24

--- android/app/src/main/java/com/shadowroot/mysteryoflimbo/MysteryOfLimboActivity.java
+++ android/app/src/main/java/com/shadowroot/mysteryoflimbo/MysteryOfLimboActivity.java
@@ -1,16 +1,24 @@
 package com.shadowroot.mysteryoflimbo;
 
 import org.libsdl.app.SDLActivity;
+import android.util.Log;
 
 public class MysteryOfLimboActivity extends SDLActivity {
 
     @Override
     protected String[] getLibraries() {
         return new String[] {
-            "SDL2",       // Cargamos el núcleo de SDL primero
-            "SDL2_mixer", // Cargamos el mezclador para el audio
-            "main"        // Finalmente tu juego, que depende de las anteriores
+            "SDL2",
+            "SDL2_mixer",
+            "main"
         };
+    }
+
+    // Sobrescribimos para añadir un log que nos ayude a debuggear en Termux con logcat
+    @Override
+    protected void onCreate(android.os.Bundle savedInstanceState) {
+        Log.d("ShadowRoot", "Iniciando Mystery of Limbo...");
+        super.onCreate(savedInstanceState);
     }
 }
 
