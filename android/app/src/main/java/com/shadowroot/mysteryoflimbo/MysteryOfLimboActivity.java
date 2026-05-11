064604d7c492ddbd5cf5eee6c6926192f6376b21
ShadowRoot07 <angstart.1109@gmail.com>
2026-05-11 16:44:02 +0000

    Feat: Alteracion de la creacion de la APK

--- android/app/src/main/java/com/shadowroot/mysteryoflimbo/MysteryOfLimboActivity.java
+++ android/app/src/main/java/com/shadowroot/mysteryoflimbo/MysteryOfLimboActivity.java
@@ -3,6 +3,7 @@ package com.shadowroot.mysteryoflimbo;
 import org.libsdl.app.SDLActivity;
 import android.util.Log;
 import android.os.Bundle;
+import android.view.View; // Necesario para los flags de visibilidad
 
 public class MysteryOfLimboActivity extends SDLActivity {
 
@@ -17,9 +18,31 @@ public class MysteryOfLimboActivity extends SDLActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        // Este log es nuestra señal de vida en logcat
         Log.i("ShadowRoot", "=== [ Mystery of Limbo: ENGINE START ] ===");
         super.onCreate(savedInstanceState);
+        
+        // Aplicamos el modo inmersivo al iniciar
+        hideSystemUI();
+    }
+
+    @Override
+    public void onWindowFocusChanged(bool hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            // Si el usuario sale de la app y vuelve, nos aseguramos de ocultar todo de nuevo
+            hideSystemUI();
+        }
+    }
+
+    private void hideSystemUI() {
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN);
     }
 }
 
